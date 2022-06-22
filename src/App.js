9a1fe7b87c687b94b3c6a06e53d5781d626572ed
Leticia-C <leticia.camposg93@gmail.com>
2022-06-22 15:48:28 -0300

    Parte do requisito LoginPage e Requisito WalletPage completo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      <header>
+        <h1>Hello, TrybeWallet!</h1>
+      </header>
+
+      <Route exact path="/" component={ Login } />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,58 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  state= {
+    isButtonValid: true,
+  }
+
+  isValid = ({ target }) => {
+    const { value, name } = target;
+    if (value.includes('@')) {
+      this.setState({ [name]: value, isButtonValid: true });
+    } else {
+      this.setState({ isButtonValid: false });
+    }
+  }
+
   render() {
-    return <div>Login</div>;
+    const { isButtonValid } = this.state;
+    return (
+      <div>
+        <form>
+          <h1> Login</h1>
+          <label htmlFor="email-input">
+            Adicione Email:
+            <input
+              name="email-input"
+              id="email-input"
+              type="email"
+              data-testid="email-input"
+              onChange={ this.isValid }
+            />
+          </label>
+          <label htmlFor="password-input">
+            Adicione Senha:
+            <input
+              name="password-input"
+              id="password-input"
+              type="password"
+              data-testid="password-input"
+              minLength={ 6 }
+            />
+          </label>
+        </form>
+        <Link to="/carteira">
+          <button
+            checked={ isButtonValid }
+            type="submit"
+            onClick={ this.isValid }
+          >
+            Entrar
+          </button>
+        </Link>
+      </div>
+    );
   }
 }
 
